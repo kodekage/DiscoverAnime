e2d2e58953d35abae63cd83a73f2120e419c0cf4
Prosper Opara <oparaprosper79@gmail.com>
2020-05-22 14:16:24 +0100

    chore: Add config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from "@chakra-ui/core";
+
 import './index.css';
+
+import theme from './theme';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
