cda53a924c6bb5399ff67aaef3cd3636a21064cc
Prosper Opara <oparaprosper79@gmail.com>
2020-05-24 00:00:26 +0100

    chore: refactor

--- src/App.js
+++ src/App.js
@@ -30,19 +30,57 @@ function App() {
   const [query, updateQuery] = useState("psycho-pass");
   const [movies, updateMovies] = useState({results: []})
   const handleChange = event => updateValue(event.target.value);
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = event => {
     updateQuery(value);
+    event.preventDefault();
   }
-  let moviesToDisplay = [];
+  const moviesToDisplay = [];
 
   useEffect(() => {
     const fetchData = async () => {
       const movies = await axios(`https://api.themoviedb.org/3/search/movie?api_key=b419b17ab95d3aeb8f0fcb1737eda29e&query=${query}`);
       updateMovies(movies.data);
     }
-    fetchData();
+    if (query) fetchData();
   }, [query])
+
+  const BasicUsage = props => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+    return (
+      <>
+        <Box h="500px" bg="#111" cursor="pointer" onClick={onOpen}>
+          <Image h="100%" w="100%" objectFit="cover" objectPosition="top" src={`https://image.tmdb.org/t/p/original/${props.poster}`} alt={props.title}/>
+        </Box>
+
+        <Modal isOpen={isOpen} onClose={onClose} size="xl">
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>{props.original_title}</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Image size="100%" objectFit="cover" objectPosition="center" src={`https://image.tmdb.org/t/p/original/${props.poster}`} alt={props.title}/>
+              <Heading as="h6" m="0px">Synopsis</Heading>
+              <Text>
+                {props.overview}
+              </Text>
+              <StatGroup>
+                <Stat>
+                  <StatLabel>Total Vote</StatLabel>
+                  <StatNumber>{props.vote}</StatNumber>
+                </Stat>
+
+                <Stat>
+                  <StatLabel as="h2">Popularity</StatLabel>
+                  <StatNumber>{props.popularity}</StatNumber>
+                </Stat>
+              </StatGroup>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      </>
+    );
+  }
 
   return (
     <Box p={2} mx="auto" textAlign="center">
@@ -58,45 +96,16 @@ function App() {
       <Heading as="h4" color="#fff">Search result for <span>{query}</span></Heading>
 
       <SimpleGrid color="white" spacing="40px" columns={[1, null, 3]} width="90%" mx="auto" my="5em">
-        {movies.results.map((item, index) => {if (item["poster_path"]) {
-          function BasicUsage() {
-            const { isOpen, onOpen, onClose } = useDisclosure();
-
-            return (
-              <>
-                <Box h="500px" bg="#111" cursor="pointer" onClick={onOpen}>
-                  <Image h="100%" w="100%" objectFit="cover" objectPosition="top" src={`https://image.tmdb.org/t/p/original/${item["poster_path"]}`} alt={item.title}/>
-                </Box>
-
-                <Modal isOpen={isOpen} onClose={onClose} size="xl">
-                  <ModalOverlay />
-                  <ModalContent>
-                    <ModalHeader>{item["original_title"]}</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                      <Image size="100%" objectFit="cover" objectPosition="center" src={`https://image.tmdb.org/t/p/original/${item["poster_path"]}`} alt={item.title}/>
-                      <Heading as="h6" m="0px">Synopsis</Heading>
-                      <Text>
-                        {item["overview"]}
-                      </Text>
-                      <StatGroup>
-                        <Stat>
-                          <StatLabel>Total Vote</StatLabel>
-                          <StatNumber>{item["vote_count"]}</StatNumber>
-                        </Stat>
-
-                        <Stat>
-                          <StatLabel as="h2">Popularity</StatLabel>
-                          <StatNumber>{item["popularity"]}</StatNumber>
-                        </Stat>
-                      </StatGroup>
-                    </ModalBody>
-                  </ModalContent>
-                </Modal>
-              </>
-            );
-          }
-         moviesToDisplay.push(<BasicUsage key={index}/>)
+        {movies.results.forEach( item => {if (item["poster_path"]) {
+          moviesToDisplay.push(<BasicUsage
+            key={item["id"]}
+            title={item["title"]}
+            original_title={item["original_title"]}
+            overview={item["overview"]}
+            poster={item["poster_path"]}
+            vote={item["vote_count"]}
+            popularity={item["popularity"]}
+          />);
         }})}
         { moviesToDisplay }
       </SimpleGrid>
